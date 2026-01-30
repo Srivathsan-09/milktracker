8774ebb59d810e8359a4d5bca1a769e5f2ede839
Srivathsan N <srivathsan0906@gmail.com>
2026-01-30 12:25:14 +0530

    Server crashed Rectified

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json());
 /* -------------------- ROUTES -------------------- */
 app.use('/api/milk', milkRoutes);
 
+/* -------------------- STATIC FILES -------------------- */
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../Front end/public')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../Front end/public', 'index.html'));
+// });
+
 /* -------------------- EXPORT (IMPORTANT) -------------------- */
 /*
 
